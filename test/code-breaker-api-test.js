274af0d19687968766440b39ea6a6bb5fae3f571
Jonathan Diosa <jadiosa@experimentality.co>
2017-10-22 05:56:52 -0500

    coverage test

--- test/code-breaker-api-test.js
+++ test/code-breaker-api-test.js
@@ -7,6 +7,7 @@ var supertest = require('supertest'),
 var request = supertest(app)
 var expect = chai.expect;
 
+/*
 describe('GET /setSecret/:number', function() {
 
     it('should return code 200', function(done) {
@@ -93,3 +94,4 @@ describe('GET /guess/:number', function() {
   			    });
     });
 });
+*/
